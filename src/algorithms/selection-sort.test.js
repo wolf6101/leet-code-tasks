ac09135587b504813a341398b560c5dc5d3f4368
Vitalii Volkov <Vitalii.Volkov@codeworldwide.com>
2021-03-13 13:04:31 +0200

    added selection sort implementation

--- src/algorithms/selection-sort.test.js
+++ src/algorithms/selection-sort.test.js
@@ -1,12 +1,13 @@
-const sort = require('./quick-sort');
+const sort = require('./selection-sort');
 
 const testData = [
     [[1, 3, 8, 2], [1, 2, 3, 8]],
     [[4, 2, 6, 5, 2], [2, 2, 4, 5, 6]],
     [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]],
+    [[1, 1, 3, 3, 4, 3, 4, 3, 4, 3, 4, 4, 1, 1], [1, 1, 1, 1, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4]],
 ];
 
-describe('quick-sort', () => {
+describe('selection-sort', () => {
     test.each(testData)('(%s) -> %s', (arr, expected) => {
         expect(sort(arr)).toEqual(expected);
     })
