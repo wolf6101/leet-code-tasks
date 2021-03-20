5fa4997112ee36372fc2bd63f52221b0451b95c4
Vitalii Volkov <Vitalii.Volkov@codeworldwide.com>
2021-03-20 13:56:02 +0200

    fixed quick find

--- src/sedgwick/union-find/implementations/quick-find.js
+++ src/sedgwick/union-find/implementations/quick-find.js
@@ -1,43 +1,42 @@
-class QuickFind {
-    constructor(size) {
-        this.arr = [];
-        for (let i = 0; i < size; i++) {
-            this.arr.push(i);
-        }
-    }
-
-    // O(1) T
-    isConnected(p, q) {
-        return this.arr[p] === this.arr[q];
-    }
-
-    // O(N) T
-    connect(p, q) {
-        const pVal = this.arr[p];
-        const qVal = this.arr[q];
-
-        if (pVal === qVal) return;
-
-        for (let i = 0; i < this.arr.length; i++) {
-            if (this.arr[i] === pVal) {
-                this.arr[i] = qVal;
-            }
-        }
-    }
-
-    // O(N) T, O(N) S
-    getNumberOfComponents() {
-        const set = new Set();
-
-        for (let el of this.arr) {
-            if (!set.has(el)) {
-                set.add(el);
+    class QuickFind {
+        constructor(size) {
+            this.arr = [];
+            for (let i = 0; i < size; i++) {
+                this.arr.push(i);
             }
         }
 
-        return set.size;
-    }
-}
+        // O(1) T
+        isConnected(p, q) {
+            return this.arr[p] === this.arr[q];
+        }
 
-module.exports = QuickFind;
-j
+        // O(N) T
+        connect(p, q) {
+            const pVal = this.arr[p];
+            const qVal = this.arr[q];
+
+            if (pVal === qVal) return;
+
+            for (let i = 0; i < this.arr.length; i++) {
+                if (this.arr[i] === pVal) {
+                    this.arr[i] = qVal;
+                }
+            }
+        }
+
+        // O(N) T, O(N) S
+        getNumberOfComponents() {
+            const set = new Set();
+
+            for (let el of this.arr) {
+                if (!set.has(el)) {
+                    set.add(el);
+                }
+            }
+
+            return set.size;
+        }
+    }
+
+    module.exports = QuickFind;
